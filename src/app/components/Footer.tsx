d45f6a9b4024876b9d213e8624bfa42cb0ec9b8b
Kaan Saydam <121751191+kaannsaydamm@users.noreply.github.com>
2025-01-10 00:46:03 +0300

    Update Footer.tsx

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
     <footer className="bg-gray-800 text-white py-4 mt-10">
       <div className="flex justify-center items-center max-w-7xl mx-auto px-4">
         <p className="text-sm">
-          Made with ❤️ by <Link href="https://github.com/atailh4n" className="text-blue-400 hover:underline">atailh4n</Link>. All rights reserved.
+          Made with ❤️ by <Link href="https://github.com/kaannsaydamm" className="text-blue-400 hover:underline">kaannsaydamm</Link>. All rights reserved.
         </p>
       </div>
     </footer>
